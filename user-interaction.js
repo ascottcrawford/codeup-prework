94885a4e22dd394e68f6a451c12b4a4773b974b7
Scott Crawford <scottcrawford@Scotts-MacBook-Air-2.local>
2016-05-11 16:43:21 -0500

    Completed pre-work and now uploading.

---
+++ user-interaction.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var userName = "";
+
+while(userName === "") {
+	userName = prompt("Hi, what is your name?")
+}
+
+console.log(userName);
+
+// TODO: Ask the user for their name.
+//       Keep asking if an empty input is provided.
+
+// TODO: Show an alert message that welcomes the user based on their input.
+
+// TODO: Ask the user if they like pizza.
+//       Based on their answer show a creative alert message.
